f847fbfeb6f5b52828d7635a4f20afe8282d30c2
Virgile <dci.virgile@gmail.com>
2019-07-08 12:38:44 +0200

    working Discord Hook

--- discordhook/assets/main.js
+++ discordhook/assets/main.js
@@ -1,11 +1,15 @@
-var go = document.getElementById('go');
 
-go.addEventListener('submit', function () {
-    var webhook = document.querySelector('#webhook');
-    var message = document.querySelector('#message');
-    message = message.value;
-    webhook = webhook.value;
-    localStorage.setItem('webhook', JSON.stringify(webhook));
-    localStorage.setItem('message', JSON.stringify(message));
-});
-document.getElementById("sent").innerHTML = "you sent: " + localStorage.getItem('message') + " to: " + localStorage.getItem('webhook');
+
+function send(event) {
+    event.preventDefault()
+    let webhook = document.getElementById('webhook').value;
+    let message = document.getElementById('message').value;
+    document.getElementById('sent').innerHTML = `You sent the message: "${message}" to "${webhook}"`;
+    message = { "username": "BlackJack", "content": message };
+    message = JSON.stringify(message);
+    req = new XMLHttpRequest();
+    req.open("POST", webhook, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.send(message);
+    
+};
